36fbca951ef7df1fd3b0f88e09f3c900aaa94a38
AnatoliyDanets <danetsxp@gmail.com>
2021-08-14 22:23:00 +0300

    Пагинация

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,78 @@
+// поиск страны
+// import countryTmpl from '../templates/country-card.hbs';
+// import countriesTmpl from '../templates/countries-list.hbs';
+// var debounce = require('lodash.debounce');
+// const cardContainer = document.querySelector('.container')
+// const counrtyList = document.querySelector('.country-list')
+// const input = document.querySelector('.inp');
+// console.log(input);
+
+// input.addEventListener('input', debounce(searchCountries, 500));
+
+// function searchCountries(e) {
+//     const searchQuery = e.target.value;
+//     fetchCountries(searchQuery)
+//         .then(countries => {
+//             if (countries.length === 1) {
+//                 const markup = countryTmpl(countries);
+//                 cardContainer.innerHTML = markup;
+//                 clearCountries(counrtyList)
+//             }
+//             else {
+//                 const markup = countriesTmpl(countries);
+//                 counrtyList.innerHTML = markup;
+//                 clearCountries(cardContainer)
+//             }
+
+//         }).catch(error => console.log(error))
+// }
+
+
+
+// function fetchCountries(name) {
+//     return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+//         .then((response) => { return response.json() })
+
+// }
+
+// function clearCountries(place) {
+//     place.innerHTML = '';
+
+// }
+
+
+
+
+
+// Пагинация
+import NewsApiService from './news-service';
+import articlesTmpl from '../templates/articles.hbs'
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    articleContainer: document.querySelector('.js-articles-container'),
+    loadBtn: document.querySelector('[data-action="load-more"]')
+}
+const newApiService = new NewsApiService();
+refs.searchForm.addEventListener('submit', onSearch);
+
+refs.loadBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+    e.preventDefault();
+    clearArticles();
+    newApiService.query = e.currentTarget.elements.query.value;
+    newApiService.resetPage();
+    newApiService.fetchArticles().then(appendArticles);
+
+}
+
+function onLoadMore() {
+    newApiService.fetchArticles().then(appendArticles);
+}
+function appendArticles(articles) {
+    refs.articleContainer.insertAdjacentHTML('beforeend', articlesTmpl(articles))
+}
+function clearArticles() {
+    refs.articleContainer.innerHTML = '';
+
+}
---
+++ src/js/news-service.js
@@ -0,0 +1,33 @@
+const API = '875e058abcba44d5a7fc87e242778b12';
+const BASE_URL = 'https://newsapi.org/v2';
+export default class NewsApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1
+    }
+    fetchArticles() {
+
+        const options = {
+            headers: {
+                Authorization: API,
+            }
+        };
+
+        const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=6 &page=${this.page}`
+        return fetch(url, options)
+            .then(response => response.json())
+            .then(data => {
+                this.page += 1;
+                return data.articles;
+            })
+    }
+    resetPage() {
+        this.page = 1
+    }
+    get query() {
+        return this.searchQuery;
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
